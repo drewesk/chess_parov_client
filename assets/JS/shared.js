50ea6f48e5c993b68fcc338a0eb5347cff1752f9
Andrew Eskenazi <drewesk@gmail.com>
2017-07-10 16:03:34 -0600

    URL update

--- assets/JS/shared.js
+++ assets/JS/shared.js
@@ -10,8 +10,8 @@ const API_URL = ifLocalURL();
 function ifLocalURL() {
   let localHost = window.location.host.indexOf('localhost') != -1;
   if (localHost) {
-    return 'http://localhost:3000'
+    return 'http://localhost:3000';
   } else {
-    alert("Error can't find port 3000");
+    return 'https://chess-parov.herokuapp.com';
   }
 }
